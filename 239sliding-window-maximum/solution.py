cfe836324035ccf439480e2a9539448fd1d139b6
xushuhui <474497097@qq.com>
2019-10-07 13:32:20 +0800

    add 239

---
+++ 239sliding-window-maximum/solution.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums: return []
+        window,res = [],[]
+        for i,x in enumerate(nums):
+            if i >= k and window[0] <= i-k:
+                window.pop(0)
+            while window and nums[window[-1]] <= x:
+                window.pop()
+            window.append(i)
+            if  i>= k-1
+                res.append(nums[window[0]])
+        return res
