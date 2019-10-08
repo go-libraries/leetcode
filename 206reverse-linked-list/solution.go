f1b635667e761f4b0e81c1261fca84f1bb877ac6
xushuhui <474497097@qq.com>
2019-10-08 17:28:39 +0800

    modify 206

--- 206reverse-linked-list/solution.go
+++ 206reverse-linked-list/solution.go
@@ -1,11 +1,35 @@
+package _06reverse_linked_list
 
 /**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
+* Definition for singly-linked list.
+*
  */
-func reverseList(head *ListNode) *ListNode {
-    
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func ReverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	var prev, cur *ListNode
+	cur = head
+	prev = nil
+	for {
+		cur.Next, prev, cur = prev, cur, cur.Next
+		if cur == nil {
+			break
+		}
+	}
+	return prev
+}
+func ReverseList2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	p := ReverseList2(head.Next)
+	head.Next.Next, head.Next = head, nil
+	return p
+
 }
---
+++ 206reverse-linked-list/solution_test.go
@@ -0,0 +1,11 @@
+package _06reverse_linked_list
+
+import (
+	"testing"
+)
+
+//不知道怎么模拟leetcode传入的数据结构
+func TestReverseList(t *testing.T) {
+
+	//t.Log(ReverseList())
+}
