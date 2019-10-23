7503c875fb1c6e8c785c8e668f641388edeb4860
xushuhui <474497097@qq.com>
2019-10-23 10:19:58 +0800

    add 102

---
+++ 102binary-tree-level-order-traversal/solution.go
@@ -0,0 +1,14 @@
+package _02binary_tree_level_order_traversal
+
+/**
+ * Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func LevelOrder(root *TreeNode) [][]int {
+
+}
---
+++ 102binary-tree-level-order-traversal/solution_test.go
@@ -0,0 +1,7 @@
+package _02binary_tree_level_order_traversal
+
+import "testing"
+
+func TestLevelOrder(t *testing.T) {
+
+}
