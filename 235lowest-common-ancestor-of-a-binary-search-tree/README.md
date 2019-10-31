fe79dbed3827b73fb00fcd3b9132ad8fd40c02ae
xushuhui <474497097@qq.com>
2019-10-31 09:39:10 +0800

    solve 235

--- 235lowest-common-ancestor-of-a-binary-search-tree/README.md
+++ 235lowest-common-ancestor-of-a-binary-search-tree/README.md
@@ -1,3 +1,4 @@
+https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree
 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
 
 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
@@ -29,6 +30,3 @@
 所有节点的值都是唯一的。
 p、q 为不同节点且均存在于给定的二叉搜索树中。
 
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
--- 235lowest-common-ancestor-of-a-binary-search-tree/solution.go
+++ 235lowest-common-ancestor-of-a-binary-search-tree/solution.go
@@ -1,1 +1,32 @@
 package _35lowest_common_ancestor_of_a_binary_search_tree
+
+/**
+ * Definition for TreeNode.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+}
+func lowestCommonAncestor2(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+		if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else if root.Val < p.Val && root.Val < q.Val {
+			root = root.Right
+		} else {
+			return root
+		}
+	}
+	return nil
+}
