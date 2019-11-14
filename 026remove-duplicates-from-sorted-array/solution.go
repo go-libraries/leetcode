2b6e2e53a5bf9df20717b01052682162eb9e26c9
xushuhui <474497097@qq.com>
2019-11-14 16:17:04 +0800

    solve 026

--- 026remove-duplicates-from-sorted-array/solution.go
+++ 026remove-duplicates-from-sorted-array/solution.go
@@ -1,5 +1,15 @@
 package _26remove_duplicates_from_sorted_array
 
 func removeDuplicates(nums []int) int {
-
+	if nums == nil {
+		return 0
+	}
+	i := 0
+	for j := 1; j < len(nums); j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
 }
