e2b90468e3cff2009652066cc1f044e036df2273
xushuhui <474497097@qq.com>
2019-11-15 16:06:23 +0800

    solve 283

--- 283move-zeroes/README.md
+++ 283move-zeroes/README.md
@@ -1,3 +1,5 @@
+https://leetcode-cn.com/problems/move-zeroes
+
 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
 
 示例:
@@ -9,6 +11,3 @@
 必须在原数组上操作，不能拷贝额外的数组。
 尽量减少操作次数。
 
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/move-zeroes
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
--- 283move-zeroes/solution.go
+++ 283move-zeroes/solution.go
@@ -1,1 +1,50 @@
 package _83move_zeroes
+
+func moveZeroes(nums []int) {
+	var noZero []int
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			noZero = append(noZero, nums[i])
+		}
+	}
+	for i := 0; i < len(noZero); i++ {
+		nums[i] = noZero[i]
+	}
+	for i := len(noZero); i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
+func moveZeroes1(nums []int) {
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	for i := k; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
+func moveZeroes2(nums []int) {
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[k], nums[i] = nums[i], nums[k]
+			k++
+		}
+	}
+
+}
+func moveZeroes3(nums []int) {
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			if k != i {
+				nums[k], nums[i] = nums[i], nums[k]
+			}
+			k++
+		}
+	}
+
+}
