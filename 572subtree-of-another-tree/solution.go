6729cf967f840915cc683eceddeb547669a37478
李文龙 <liwenlong@himango.cn>
2020-05-07 11:35:48 +0800

    572

---
+++ 572subtree-of-another-tree/solution.go
@@ -0,0 +1,28 @@
+package _72subtree_of_another_tree
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func isSubtree(s *TreeNode, t *TreeNode) bool {
+	if s == nil {
+		return false
+	}
+	return check(s, t) || isSubtree(s.Left, t) || isSubtree(s.Right, t)
+}
+
+func check(a, b *TreeNode) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	if a.Val == b.Val {
+		return check(a.Left, b.Left) && check(a.Right, b.Right)
+	}
+	return false
+}
+
