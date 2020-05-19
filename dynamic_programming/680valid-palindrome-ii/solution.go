f53706822f5b80294c865d2426fa9d7e805fe476
李文龙 <liwenlong@himango.cn>
2020-05-19 11:24:49 +0800

    680

---
+++ dynamic_programming/680valid-palindrome-ii/solution.go
@@ -0,0 +1,29 @@
+package _80valid_palindrome_ii
+
+
+func validPalindrome(s string) bool {
+	left :=0
+	right :=len(s)-1
+	for left<right{
+		if s[left]!=s[right]{
+			return isPalindrome(s[left:right])||isPalindrome(s[left+1:right+1])
+		}
+		left++
+		right--
+	}
+	return true
+}
+
+func isPalindrome(s string)bool{
+	left :=0
+	right :=len(s)-1
+	for left<right{
+		if s[left]!=s[right]{
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
+
